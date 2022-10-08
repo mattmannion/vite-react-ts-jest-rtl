9f4710883253fee516c366bb6ef1eee5dbc2d2dc
Matt Mannion <mattpmannion@gmail.com>
2022-10-08 05:49:51 -0400

    more fa stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAddressCard } from '@fortawesome/free-regular-svg-icons';
+import {
+  faAddressCard,
+  faCircle,
+  faSun,
+} from '@fortawesome/free-regular-svg-icons';
 import { useState } from 'react';
+import {
+  faAtom,
+  faCircleArrowDown,
+  faCircleNodes,
+  faCircleNotch,
+  faRotate,
+  faSpinner,
+} from '@fortawesome/free-solid-svg-icons';
 
 enum Tabs {
   a = 'a',
@@ -25,6 +37,12 @@ export function App() {
         <FontAwesomeIcon
           icon={faAddressCard}
           size={'2x'}
+          style={{ color: 'black' }}
+        />
+        <FontAwesomeIcon
+          icon={faSun}
+          size={'2x'}
+          className='spin'
           style={{ color: 'black' }}
         />
       </div>

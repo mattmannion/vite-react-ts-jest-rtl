ee70b393b0239e3ba4671627514a67428b99db73
Matt Mannion <mattpmannion@gmail.com>
2022-10-21 23:56:25 -0400

    more fa stuff

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from 'src/App';
 
-createRoot(document.getElementById('root') as HTMLElement).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
